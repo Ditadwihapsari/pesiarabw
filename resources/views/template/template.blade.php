bb5ae508baf85fb38ffea97fe3bdf9a222a61c49
Ditadwihapsari <ditadwihapsari.olim@gmail.com>
2017-11-29 18:48:19 +0700

    guide register try

--- resources/views/template/template.blade.php
+++ resources/views/template/template.blade.php
@@ -60,7 +60,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 					<div class="header-right-agileits">
 					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 						<ul class="nav navbar-nav navbar-right">
-							<li><a href="guide">Become a Guide</a></li>
+							<li><a href="/pesiar/public/guide">Become a Guide</a></li>
 							<li><a href="#about">Sign in</a></li>
 							<li><a href="#team">Help</a></li>
 						</ul>
